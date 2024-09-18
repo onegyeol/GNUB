638e1599f4227073344661872993f0d39749d77b
hangyeol <chlgksruf3414@gmail.com>
2024-09-18 22:26:12 +0900

    학과 조회회

--- src/main/java/pbl/GNUB/repository/DepartmentRepository.java
+++ src/main/java/pbl/GNUB/repository/DepartmentRepository.java
@@ -8,6 +8,5 @@ import pbl.GNUB.entity.College;
 import pbl.GNUB.entity.Department;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long>{
-    List<Department> findByCollege(College college);
-
+    List<Department> findByCollege(College college); // 단과대 이름으로 해당 학과 찾기위한 메소드
 }

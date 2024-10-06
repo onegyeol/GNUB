66c9a532f28c1077c221fa8da9826d73b89f1166
hangyeol <chlgksruf3414@gmail.com>
2024-10-07 00:16:40 +0900

    메인 및 검색, 음식점 상세 페이지 url 매핑 컨트롤러

--- src/main/java/pbl/GNUB/controller/MainController.java
+++ src/main/java/pbl/GNUB/controller/MainController.java
@@ -1,16 +1,37 @@
 package pbl.GNUB.controller;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
 
 @Controller
 public class MainController {
 
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job csvShopJob; // Job 이름에 맞게 수정하세요.
+
     @GetMapping("/main")
     public String homePage() {
-        return "form/main";
+        try {
+            // JobParameters를 생성합니다. 필요한 경우 추가 파라미터를 설정할 수 있습니다.
+            JobParameters params = new JobParametersBuilder()
+                    .addLong("time", System.currentTimeMillis())
+                    .toJobParameters();
+            
+            // Job을 실행합니다.
+            jobLauncher.run(csvShopJob, params);
+        } catch (Exception e) {
+            e.printStackTrace();
+            // 에러 처리
+        }
+        return "form/main"; // 반환할 뷰 이름
     }
     
     @GetMapping("/search") // 검색결과 페이지

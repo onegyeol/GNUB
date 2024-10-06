2e119d14991617eec8467d054b2c0a9aa3508540
hangyeol <chlgksruf3414@gmail.com>
2024-10-07 00:15:54 +0900

    읽어들인 csv 파일 저장

---
+++ src/main/java/pbl/GNUB/csv/CsvShopWriter.java
@@ -0,0 +1,36 @@
+package pbl.GNUB.csv;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import pbl.GNUB.dto.ShopDto;
+import pbl.GNUB.entity.Shop;
+import pbl.GNUB.repository.ShopRepository;
+
+@Configuration
+@RequiredArgsConstructor
+public class CsvShopWriter implements ItemWriter<ShopDto> {
+
+    private final ShopRepository shopRepository;
+
+    @Override
+    @Transactional
+    public void write(Chunk<? extends ShopDto> items) throws Exception {
+        List<Shop> shopList = new ArrayList<>();
+
+        // items에서 ShopDto를 추출하여 Shop 엔티티로 변환
+        for (ShopDto dto : items) {
+            Shop shop = dto.toEntity();
+            shopList.add(shop);
+        }
+
+        // Shop 엔티티를 데이터베이스에 저장
+        shopRepository.saveAll(shopList);
+    }
+}

47026e3a8a058465d152e828410bc33fa32bae84
hangyeol <chlgksruf3414@gmail.com>
2024-10-12 23:06:03 +0900

    좋아요 dto 객체

---
+++ src/main/java/pbl/GNUB/dto/LikeDto.java
@@ -0,0 +1,12 @@
+package pbl.GNUB.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class LikeDto {
+    private Long shopId; // 음식점 Id
+    private String email; // 회원 이메일
+
+    
+}

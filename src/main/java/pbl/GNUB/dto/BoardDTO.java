e96648932455d9731e4fe4ffd0fb8a54c58e8ced
Jaeseon13 <limjaesun16@naver.com>
2024-10-22 16:51:37 +0900

    boardDTO

---
+++ src/main/java/pbl/GNUB/dto/BoardDTO.java
@@ -0,0 +1,39 @@
+package pbl.GNUB.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import pbl.GNUB.entity.Board;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class BoardDTO {
+    private Long authorId;
+    private Long id;
+    private String title;
+    private String content;
+    private int boardHits;  // 조회수
+    private LocalDateTime boardCreatedTime;  // 작성한 시간
+    private LocalDateTime boardUpdatedTime;  // 수정한 시간
+
+    public static BoardDTO toBoardDTO(Board board){
+        BoardDTO boardDTO = new BoardDTO();
+        boardDTO.setAuthorId(board.getAuthor().getId());
+        boardDTO.setId(board.getId());
+        boardDTO.setTitle(board.getTitle());
+        boardDTO.setContent(board.getContent());
+        boardDTO.setBoardHits(board.getBoardHits());
+        boardDTO.setBoardCreatedTime(board.getCreatedTime());
+        boardDTO.setBoardUpdatedTime(board.getUpdatedTime());
+        return boardDTO;
+    }
+
+}

d9aa4154a45cfdd48237440119375728ae49ba54
hangyeol <chlgksruf3414@gmail.com>
2024-10-24 14:31:16 +0900

    음식점 service

---
+++ src/main/java/pbl/GNUB/service/ShopService.java
@@ -0,0 +1,20 @@
+package pbl.GNUB.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pbl.GNUB.entity.Shop;
+import pbl.GNUB.repository.ShopRepository;
+
+@Service
+public class ShopService {
+    @Autowired
+    private ShopRepository shopRepository;
+
+    public List<Shop> getTop30Shops() {
+        // id가 1부터 30까지인 데이터를 불러오기
+        return shopRepository.findByIdBetween(1L, 30L);
+    }
+}

655d3bba54c5ec14dbb148a507c1bbf0e84e346f
hangyeol <chlgksruf3414@gmail.com>
2024-11-09 21:57:15 +0900

    commit

--- src/main/java/pbl/GNUB/repository/MemberRepository.java
+++ src/main/java/pbl/GNUB/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package pbl.GNUB.repository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import pbl.GNUB.entity.Like;
 import pbl.GNUB.entity.Member;

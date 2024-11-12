b923f1841097cb65e306267679409509298d4ed9
unknown <tjdrhkd7226@office.gntech.ac.kr>
2024-11-12 10:58:37 +0900

    음식점 태그 controller

---
+++ src/main/java/pbl/GNUB/controller/TagController.java
@@ -0,0 +1,61 @@
+package pbl.GNUB.controller;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import pbl.GNUB.entity.ShopTag;
+import pbl.GNUB.repository.ShopTagRepository;
+
+
+@Service
+public class TagController {
+
+    private final JobLauncher jobLauncher;
+    private final Job csvShopTagJob;
+    private final ShopTagRepository shopTagRepository;
+
+    @Autowired
+    public TagController(JobLauncher jobLauncher, Job csvShopTagJob, ShopTagRepository shopTagRepository) {
+        this.jobLauncher = jobLauncher;
+        this.csvShopTagJob = csvShopTagJob;
+        this.shopTagRepository = shopTagRepository;
+    }
+
+    @GetMapping("/tags")
+    public String showTags(Model model) {
+        List<ShopTag> shopTags = shopTagRepository.findTop10ByOrderByIdAsc();
+        model.addAttribute("shopTags", shopTags);
+        return "form/tags";
+    }
+
+    @GetMapping("/importTags")
+    public String importTagsFromCsv() {
+        try {
+            JobParameters params = new JobParametersBuilder()
+                    .addLong("time", System.currentTimeMillis())
+                    .toJobParameters();
+            
+            jobLauncher.run(csvShopTagJob, params);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return "form/main";
+    }
+
+    public Map<String, ShopTag> getShopTagsMap() {
+        return shopTagRepository.findAll().stream()
+            .collect(Collectors.toMap(ShopTag::getName, Function.identity(), (existing, replacement) -> existing));
+    }
+}

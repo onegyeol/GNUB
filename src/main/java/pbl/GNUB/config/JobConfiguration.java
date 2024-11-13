ef6828cd57fd07f76943499d1b81812ece52565f
hangyeol <chlgksruf3414@gmail.com>
2024-11-13 11:42:16 +0900

    JobConfiguration

--- src/main/java/pbl/GNUB/config/JobConfiguration.java
+++ src/main/java/pbl/GNUB/config/JobConfiguration.java
@@ -12,30 +12,46 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Step;
 
 import pbl.GNUB.csv.CsvShopReader;
+import pbl.GNUB.csv.CsvShopTagReader;
+import pbl.GNUB.csv.CsvShopTagWriter;
 import pbl.GNUB.csv.CsvShopWriter;
 import pbl.GNUB.dto.ShopDto;
+import pbl.GNUB.dto.ShopTagDto;
 
 @Configuration
 @RequiredArgsConstructor
 public class JobConfiguration {
-    
+
     private final CsvShopReader csvShopReader;
     private final CsvShopWriter csvShopWriter;
+    private final CsvShopTagReader csvShopTagReader;
+    private final CsvShopTagWriter csvShopTagWriter;
 
     @Bean
-    public Job csvShopJob(JobRepository jobRepository, Step shopDataLoadStep){
+    public Job csvShopJob(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
         return new JobBuilder("csvShopJob", jobRepository)
-            .start(shopDataLoadStep)
+            .start(csvShopReaderStep(jobRepository, platformTransactionManager))
+            .next(csvShopTagReaderStep(jobRepository, platformTransactionManager))
             .build();
     }
 
     @Bean
-    public Step csvShopReaderStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager){
+    public Step csvShopReaderStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
         return new StepBuilder("csvShopReaderStep", jobRepository)
             .<ShopDto, ShopDto>chunk(100, platformTransactionManager)
             .reader(csvShopReader.csvScheduleReader())
             .writer(csvShopWriter)
-            .allowStartIfComplete(true) // step 다시 실행하고 싶을 때
+            .allowStartIfComplete(true)
+            .build();
+    }
+
+    @Bean
+    public Step csvShopTagReaderStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("csvShopTagReaderStep", jobRepository)
+            .<ShopTagDto, ShopTagDto>chunk(100, platformTransactionManager)
+            .reader(csvShopTagReader.csvTagReader())
+            .writer(csvShopTagWriter)
+            .allowStartIfComplete(true)
             .build();
     }
 }

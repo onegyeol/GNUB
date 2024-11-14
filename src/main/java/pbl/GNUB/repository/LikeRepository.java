1450f6f4e70395ebd87a594b966db30b44c2b669
hangyeol <chlgksruf3414@gmail.com>
2024-11-14 12:28:53 +0900

    좋아요 Repository

--- src/main/java/pbl/GNUB/repository/LikeRepository.java
+++ src/main/java/pbl/GNUB/repository/LikeRepository.java
@@ -10,5 +10,6 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
     boolean existsByShopIdAndEmail(Long shopId, String email); // 이미 좋아요햔 음식점인지
     void deleteByShopIdAndEmail(Long shopId, String email); // 좋아요 삭제
     List<Like> findByShopId(Long shopId); // 음식점 좋아요 조회
-    List<Like> findByMember(Member member);
+    List<Like> findByMember(Member member); // 멤버 조회 
+    List<Like> findByMemberEmail(String memberEmail);
 }

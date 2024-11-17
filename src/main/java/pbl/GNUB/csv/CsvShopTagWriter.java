19924b1bc411b6aa955883801749467381e286fc
unknown <tjdrhkd7226@office.gntech.ac.kr>
2024-11-17 14:34:30 +0900

    태그 쓰는거

--- src/main/java/pbl/GNUB/csv/CsvShopTagWriter.java
+++ src/main/java/pbl/GNUB/csv/CsvShopTagWriter.java
@@ -1,38 +1,39 @@
 package pbl.GNUB.csv;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.Transactional;
 
-import lombok.RequiredArgsConstructor;
 import pbl.GNUB.dto.ShopTagDto;
 import pbl.GNUB.entity.ShopTag;
-import pbl.GNUB.repository.ShopRepository;
 import pbl.GNUB.repository.ShopTagRepository;
 
 
 @Configuration
-@RequiredArgsConstructor
 public class CsvShopTagWriter implements ItemWriter<ShopTagDto> {
     
     private final ShopTagRepository shopTagRepository;
 
+    public CsvShopTagWriter(ShopTagRepository shopTagRepository) {
+        this.shopTagRepository = shopTagRepository;
+    }
+
     @Override
     @Transactional
     public void write(Chunk<? extends ShopTagDto> items) throws Exception {
-        List<ShopTag> shopTagList = new ArrayList<>();
-
         for(ShopTagDto dto : items){
-            ShopTag shopTag = dto.toEntity();
-            shopTagList.add(shopTag);
+            ShopTag existingTag = shopTagRepository.findByAllFields(
+                dto.getName(), dto.getHygiene(), dto.getRevisit(),
+                dto.getRecent(), dto.getDelicious(), dto.getGoodValue(),
+                dto.getMood(), dto.getFresh(), dto.getKindness(),
+                dto.getAlone(), dto.getChilamDong(), dto.getGajwaDong()
+            );
+            if(existingTag == null) {
+                ShopTag newTag = dto.toEntity();
+                shopTagRepository.save(newTag);
+            }
         }
-
-        shopTagRepository.saveAll(shopTagList);
     }
 
-    
 }

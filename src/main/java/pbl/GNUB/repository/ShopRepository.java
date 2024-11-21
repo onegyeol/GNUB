7c6926b5774ab5dc38e753c5ea8767c6cb1adea0
hangyeol <chlgksruf3414@gmail.com>
2024-11-21 11:44:31 +0900

    음식점 repository

--- src/main/java/pbl/GNUB/repository/ShopRepository.java
+++ src/main/java/pbl/GNUB/repository/ShopRepository.java
@@ -9,13 +9,29 @@ import org.springframework.data.repository.query.Param;
 import pbl.GNUB.entity.Shop;
 
 public interface ShopRepository extends JpaRepository<Shop, Long>{
-   // id가 1부터 30까지인 데이터를 조회
-   List<Shop> findByIdBetween(Long startId, Long endId);
-   //List<Shop> findTop30ByOrderByLikeCountDesc();  // 좋아요 수 기준으로 30개 가져오기
-   Shop getShopById(Long id); // 해당 i로 음식점 조회
+      // id가 1부터 30까지인 데이터를 조회
+      List<Shop> findByIdBetween(Long startId, Long endId);
+      //List<Shop> findTop30ByOrderByLikeCountDesc();  // 좋아요 수 기준으로 30개 가져오기
+      Shop getShopById(Long id); // 해당 i로 음식점 조회
 
-   @Query("SELECT s FROM Shop s WHERE "
-         + "LOWER(s.name) LIKE LOWER(CONCAT('%', :query, '%')) OR "
-         + "LOWER(s.mainMenu) LIKE LOWER(CONCAT('%', :query, '%'))")
-   List<Shop> searchShops(@Param("query") String query);
+      @Query("SELECT s FROM Shop s WHERE "
+            + "LOWER(s.name) LIKE LOWER(CONCAT('%', :query, '%')) OR "
+            + "LOWER(s.mainMenu) LIKE LOWER(CONCAT('%', :query, '%'))")
+      List<Shop> searchShops(@Param("query") String query);
+
+      @Query("SELECT DISTINCT s FROM Shop s JOIN s.shopTags t WHERE " +
+            "(:tag = 'hygiene' AND t.hygiene = 1) OR " +
+            "(:tag = 'revisit' AND t.revisit = 1) OR " +
+            "(:tag = 'recent' AND t.recent = 1) OR " +
+            "(:tag = 'delicious' AND t.delicious = 1) OR " +
+            "(:tag = 'good_value' AND t.goodValue = 1) OR " +
+            "(:tag = 'mood' AND t.mood = 1) OR " +
+            "(:tag = 'fresh' AND t.fresh = 1) OR " +
+            "(:tag = 'kindness' AND t.kindness = 1) OR " +
+            "(:tag = 'alone' AND t.alone = 1) OR " +
+            "(:tag = 'chilam_dong' AND t.chilamDong = 1) OR " +
+            "(:tag = 'gajwa_dong' AND t.gajwaDong = 1)" +
+            " AND (:query IS NULL OR :query = '' OR LOWER(s.name) LIKE LOWER(CONCAT('%', :query, '%')))")
+      List<Shop> findShopsByDynamicTag(@Param("tag") String tag, @Param("query") String query);
+
 }

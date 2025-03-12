a7e970eaa4cb5dad5c5eedb37de1d1535c01d54f
hangyeol <chlgksruf3414@gmail.com>
2025-03-12 16:28:28 +0900

    jobconfiguration

--- src/main/java/pbl/GNUB/config/JobConfiguration.java
+++ src/main/java/pbl/GNUB/config/JobConfiguration.java
@@ -1,5 +1,6 @@
 package pbl.GNUB.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
@@ -8,14 +9,8 @@ import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
-
-import lombok.RequiredArgsConstructor;
-import pbl.GNUB.csv.CsvShopReader;
-import pbl.GNUB.csv.CsvShopTagReader;
-import pbl.GNUB.csv.CsvShopTagWriter;
-import pbl.GNUB.csv.CsvShopWriter;
-import pbl.GNUB.dto.ShopDto;
-import pbl.GNUB.dto.ShopTagDto;
+import pbl.GNUB.csv.*;
+import pbl.GNUB.dto.*;
 
 @Configuration
 @RequiredArgsConstructor
@@ -25,19 +20,24 @@ public class JobConfiguration {
     private final CsvShopWriter csvShopWriter;
     private final CsvShopTagReader csvShopTagReader;
     private final CsvShopTagWriter csvShopTagWriter;
+    private final CsvShopMenuReader csvMenuReader;
+    private final CsvShopMenuWriter csvMenuWriter;
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
 
     @Bean
-    public Job csvShopJob(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+    public Job csvShopJob() {
         return new JobBuilder("csvShopJob", jobRepository)
-            .start(csvShopReaderStep(jobRepository, platformTransactionManager))
-            .next(csvShopTagReaderStep(jobRepository, platformTransactionManager))
+            .start(csvShopReaderStep())
+            .next(csvShopTagReaderStep())
+            .next(csvShopMenuReaderStep())
             .build();
     }
 
     @Bean
-    public Step csvShopReaderStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+    public Step csvShopReaderStep() {
         return new StepBuilder("csvShopReaderStep", jobRepository)
-            .<ShopDto, ShopDto>chunk(100, platformTransactionManager)
+            .<ShopDto, ShopDto>chunk(100, transactionManager)
             .reader(csvShopReader.csvScheduleReader())
             .writer(csvShopWriter)
             .allowStartIfComplete(true)
@@ -45,11 +45,21 @@ public class JobConfiguration {
     }
 
     @Bean
-    public Step csvShopTagReaderStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+    public Step csvShopTagReaderStep() {
         return new StepBuilder("csvShopTagReaderStep", jobRepository)
-            .<ShopTagDto, ShopTagDto>chunk(1000, platformTransactionManager)
+            .<ShopTagDto, ShopTagDto>chunk(1000, transactionManager)
             .reader(csvShopTagReader.csvTagReader())
             .writer(csvShopTagWriter)
+            .allowStartIfComplete(true)
+            .build();
+    }
+
+    @Bean
+    public Step csvShopMenuReaderStep() {
+        return new StepBuilder("csvShopMenuReaderStep", jobRepository)
+            .<ShopMenuDto, ShopMenuDto>chunk(1000, transactionManager)
+            .reader(csvMenuReader.csvShopMenuReader())
+            .writer(csvMenuWriter)
             .allowStartIfComplete(true)
             .build();
     }

06572c7144c1888ba6f8ebfe0164d2d0d7383359
hangyeol <chlgksruf3414@gmail.com>
2025-03-12 16:21:58 +0900

    BatchConfig

---
+++ src/main/java/pbl/GNUB/config/BatchConfig.java
@@ -0,0 +1,51 @@
+package pbl.GNUB.config;
+
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.TaskExecutorJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class BatchConfig {
+
+    private final DataSource dataSource;
+    private final PlatformTransactionManager transactionManager;
+
+    @Autowired
+    public BatchConfig(DataSource dataSource, PlatformTransactionManager transactionManager) {
+        this.dataSource = dataSource;
+        this.transactionManager = transactionManager;
+    }
+
+    @Bean
+    public JobRepository jobRepository() throws Exception {
+        JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
+        factory.setDataSource(dataSource);
+        factory.setTransactionManager(transactionManager);
+        factory.setDatabaseType("mysql");
+        factory.afterPropertiesSet();
+        return factory.getObject();
+    }
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        return new SimpleAsyncTaskExecutor(); // 비동기 실행을 위한 TaskExecutor
+    }
+
+    @Bean
+    public JobLauncher jobLauncher() throws Exception {
+        TaskExecutorJobLauncher launcher = new TaskExecutorJobLauncher();
+        launcher.setJobRepository(jobRepository());
+        launcher.setTaskExecutor(taskExecutor()); // 비동기 실행 적용
+        launcher.afterPropertiesSet();
+        return launcher;
+    }
+}

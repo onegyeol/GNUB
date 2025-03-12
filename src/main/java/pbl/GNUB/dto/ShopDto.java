b8f764475b6ac7b17a40c2837cf9cd4a208116d2
hangyeol <chlgksruf3414@gmail.com>
2025-03-12 16:33:56 +0900

    음식점 dto

--- src/main/java/pbl/GNUB/dto/ShopDto.java
+++ src/main/java/pbl/GNUB/dto/ShopDto.java
@@ -17,50 +17,78 @@ import pbl.GNUB.entity.Shop;
 @ApiModel(value = "음식점정보")
 public class ShopDto {
 
-    @ApiModelProperty(name = "시도")
-    private String region; // 시도 
+    @ApiModelProperty(name = "rest")
+    private String name; // 업소명 
 
-    @ApiModelProperty(name = "시군구")
-    private String city; // 시군구
-
-    @ApiModelProperty(name = "업태")
+    @ApiModelProperty(name = "type")
     private String category; // 업태
 
-    @ApiModelProperty(name = "주메뉴")
-    private String mainMenu; // 주메뉴
-
-    @ApiModelProperty(name = "업소명")
-    private String name; // 업소명
-
-    @ApiModelProperty(name = "소재지도")
+    @ApiModelProperty(name = "address")
     @Column(columnDefinition = "VARCHAR(255) CHARACTER SET utf8mb4")
     private String address; // 주소
 
-    @ApiModelProperty(name = "전화번호")
-    private String phone; // 전화번호
+    @ApiModelProperty(name = "address_info")
+    private String addressInfo; // 주메뉴
 
-    @ApiModelProperty(name = "위도")
-    private Double lat; // 위도
+    @ApiModelProperty(name = "number")
+    private String number; // 전화번호
 
-    @ApiModelProperty(name = "경도")
-    private Double lng; // 경도
+    @ApiModelProperty(name = "site")
+    private String site; // 해당 사이트
+
+    @ApiModelProperty(name = "info")
+    private String info; // 가게 정보
 
     @ApiModelProperty(name = "img_url")
     @Column(columnDefinition = "TEXT")
     private String imgUrl;
 
+    @ApiModelProperty(name = "Mon")
+    private String mon; // 월요일
+
+    @ApiModelProperty(name = "Tue")
+    private String tue; // 화요일
+
+    @ApiModelProperty(name = "Wen")
+    private String wen; // 수요일
+
+    @ApiModelProperty(name = "Thu")
+    private String thu; // 목요일
+
+    @ApiModelProperty(name = "Fri")
+    private String fri; // 금요일
+
+    @ApiModelProperty(name = "Sat")
+    private String sat; // 토요일
+
+    @ApiModelProperty(name = "Sun")
+    private String sun; // 일요일
+
+    @ApiModelProperty(name = "lat")
+    private Double lat; // 위도
+
+    @ApiModelProperty(name = "lng")
+    private Double lng; // 경도
+
     public Shop toEntity(){ //Shop 엔티티 반환
         return Shop.builder()
-            .region(this.region)
-            .city(this.city)
-            .category(this.category)
-            .mainMenu(this.mainMenu)
             .name(this.name)
+            .category(this.category)
             .address(this.address)
-            .phone(this.phone)
+            .addressInfo(this.addressInfo)
+            .number(this.number)
+            .site(this.site)
+            .info(this.info)
+            .imgUrl(this.imgUrl)
+            .mon(this.mon)
+            .tue(this.tue)
+            .wen(this.wen)
+            .thu(this.thu)
+            .fri(this.fri)
+            .sat(this.sat)
+            .sun(this.sun)
             .lat(this.lat)
             .lng(this.lng)
-            .imgUrl(this.imgUrl)
             .build();
     }
 

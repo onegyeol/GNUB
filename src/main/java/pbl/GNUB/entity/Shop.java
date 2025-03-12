41ea1c9f1bdfb2c114393b194c8219c77070ace0
hangyeol <chlgksruf3414@gmail.com>
2025-03-12 16:33:37 +0900

    음식점 entity

--- src/main/java/pbl/GNUB/entity/Shop.java
+++ src/main/java/pbl/GNUB/entity/Shop.java
@@ -2,8 +2,10 @@ package pbl.GNUB.entity;
 
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -31,23 +33,33 @@ public class Shop {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // id
 
-    private String region; // 시도 
-    private String city; // 시군구
-    private String category; // 업태
-    
-    @Column(name = "main_menu", length = 255)
-    private String mainMenu; // 주메뉴
-    
     private String name; // 업소명
-    private String address; // 소재지도
-    private String phone; // 전화번호
-    private Double lat; // 위도
-    private Double lng; // 경도
+    private String category; // 업태
+    private String address; // 주소
+    private String addressInfo;
+    private String number; // 전화번호
+    private String site; // 해당 사이트
+    private String info; 
 
     @Column(length = 1024)  // 이미지 URL 길이를 1024자로 제한
     private String imgUrl; // 이미지 Url
-    
+
+    private String mon;
+    private String tue;
+    private String wen;
+    private String thu;
+    private String fri;
+    private String sat;
+    private String sun; 
+
+    private Double lat; // 위도
+    private Double lng; // 경도
+
+    @Builder.Default
     private int likeCount = 0; // 좋아요 수 카운트
+
+    @OneToMany(mappedBy = "shop", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<ShopMenu> shopMenus;
 
     @ManyToMany
     @JoinTable(
@@ -62,8 +74,4 @@ public class Shop {
         this.likeCount = likeCount;
     }
 
-    // setter 메서드 추가
-    public void setMainMenu(String mainMenu) {
-        this.mainMenu = mainMenu;
-    }
 }

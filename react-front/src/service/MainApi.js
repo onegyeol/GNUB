9fbf13f7927397f157789b5cecaf4bc9c4d86cc1
hangyeol <chlgksruf3414@gmail.com>
2025-05-07 21:06:55 +0900

    mainApi

--- react-front/src/service/MainApi.js
+++ react-front/src/service/MainApi.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const BASE_URL = 'http://localhost:8080/api/main'; // 스프링 서버 주소
 
-// categorizedShops API 호출 함수
-export const fetchCategorizedShops = async () => {
-  const response = await axios.get(`${BASE_URL}/shops`);
+
+export const fetchTaggedShops = async () => {
+  const response = await axios.get(`${BASE_URL}/tags`);
   return response.data;
 };

cb991e0123bf7ce5fd98b14c6b4d15f42e073f55
hangyeol <chlgksruf3414@gmail.com>
2025-05-07 21:03:04 +0900

    BoardApi js

---
+++ react-front/src/service/BoardApi.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const BASE = 'http://localhost:8080/api/boards';
+
+export const fetchBoardList = ({ query = '', sort = 'created', page = 1 }) =>
+  axios
+    .get(BASE, {
+      params: { query, sort, page },
+      withCredentials: true,
+    })
+    .then(res => res.data);
+
+
+export const fetchBoardById = async (id) => {
+  try {
+    const response = await fetch(`${BASE}/${id}`);
+    if (!response.ok) {
+      throw new Error('게시글을 불러오는 데 실패했습니다.');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('❌ 게시글 API 오류:', error);
+    throw error;
+  }
+};

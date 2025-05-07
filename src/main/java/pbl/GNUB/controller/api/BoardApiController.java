0d37e48ec28387c4e6adaf7d2f1f241f7f7301ae
hangyeol <chlgksruf3414@gmail.com>
2025-05-07 21:07:33 +0900

    BoardApiController

--- src/main/java/pbl/GNUB/controller/api/BoardApiController.java
+++ src/main/java/pbl/GNUB/controller/api/BoardApiController.java
@@ -2,9 +2,11 @@ package pbl.GNUB.controller.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pbl.GNUB.dto.BoardDto;
 import pbl.GNUB.service.BoardService;
+
 
 @RestController
 @RequestMapping("/api/boards")
@@ -27,4 +29,16 @@ public class BoardApiController {
             return boardService.findPaginatedWithSort(page, pageSize, sort);
         }
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BoardDto> getBoard(@PathVariable Long id) {
+        BoardDto boardDto = boardService.getBoardById(id);
+
+        // Zero Width No-Break Space 제거
+        String cleanedContent = boardDto.getContent().replaceAll("\uFEFF", "");
+        boardDto.setContent(cleanedContent);
+
+        return ResponseEntity.ok(boardDto);
+    }
+    
 }

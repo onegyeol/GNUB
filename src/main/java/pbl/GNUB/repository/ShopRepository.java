e95b47d72624ec66ca96e163a3b683bdd1953d5b
hangyeol <chlgksruf3414@gmail.com>
2025-05-19 13:29:40 +0900

    shop Respository

--- src/main/java/pbl/GNUB/repository/ShopRepository.java
+++ src/main/java/pbl/GNUB/repository/ShopRepository.java
@@ -2,6 +2,7 @@ package pbl.GNUB.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;

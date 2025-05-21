8ec5e8a7fa267c8a33907a856454a2f54750073d
Jaeseon13 <limjaesun16@naver.com>
2025-05-21 15:45:54 +0900

    boarddetails.js

--- react-front/src/pages/BoardDetails.js
+++ react-front/src/pages/BoardDetails.js
@@ -88,7 +88,6 @@ const BoardDetails = () => {
               <span>매거진</span>
             </Link>
 
-            <a href="/" className="nav-item">
             <Link to="/" className="nav-item">
               <svg className="nav-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M3 9.5L12 3l9 6.5V21a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1v-5H10v5a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9.5z" />

5205d6abe0ed8308d733d5de0f4c59ac21f6ddaf
hangyeol <chlgksruf3414@gmail.com>
2025-05-21 19:41:02 +0900

    bookmarkApi

--- react-front/src/service/BookmarkApi.js
+++ react-front/src/service/BookmarkApi.js
@@ -3,28 +3,28 @@ import axios from 'axios';
 const BASE = process.env.REACT_APP_API_URL;
 
 export const fetchFolders = () =>
-  axios.get(`${BASE}/folders`, { withCredentials: true }).then(res => res.data);
+  axios.get(`${BASE}/api/bookmarks/folders`, { withCredentials: true }).then(res => res.data);
 
 export const saveBookmark = (shopId, folderId = null) =>
-  axios.post(`${BASE}`, null, {
+  axios.post(`${BASE}/api/bookmarks`, null, {
     params: { shopId, folderId },
     withCredentials: true,
   });
 
 export const deleteBookmark = (shopId) =>
-  axios.delete(`${BASE}`, {
+  axios.delete(`${BASE}/api/bookmarks/`, {
     params: { shopId },
     withCredentials: true,
   });
 
 export const createFolder = (name) =>
-  axios.post(`${BASE}/folders`, null, {
+  axios.post(`${BASE}/api/bookmarks/folders`, null, {
     params: { name },
     withCredentials: true,
   });
 
 export const deleteFolder = (name) =>
-  axios.delete(`${BASE}/folders`, {
+  axios.delete(`${BASE}/api/bookmarks/folders`, {
     params: { name },
     withCredentials: true,
   });

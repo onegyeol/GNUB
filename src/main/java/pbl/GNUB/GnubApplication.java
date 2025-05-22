5d5f31bb9ec736fa2c5ff4ad28bb14fb71dfc521
BBoorinkeAngel <tjdrhkd7226@naver.com>
2025-05-22 12:02:31 +0900

    Update GnubApplication.java

--- src/main/java/pbl/GNUB/GnubApplication.java
+++ src/main/java/pbl/GNUB/GnubApplication.java
@@ -3,11 +3,13 @@ package pbl.GNUB;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableBatchProcessing // Spring Batch 활성화
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class) // security config 로그인 화면 없앰
+@EntityScan(basePackages = "pbl.GNUB.entity")
 public class GnubApplication {
 
     public static void main(String[] args) {

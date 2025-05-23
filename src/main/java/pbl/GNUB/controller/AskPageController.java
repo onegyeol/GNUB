27b2680740d2615d76a8a28b6b19510efe5f2abb
hangyeol <83857296+onegyeol@users.noreply.github.com>
2025-05-24 01:44:48 +0900

    Update AskPageController.java

--- src/main/java/pbl/GNUB/controller/AskPageController.java
+++ src/main/java/pbl/GNUB/controller/AskPageController.java
@@ -6,5 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AskPageController {
 
-    
+    @GetMapping("/ask")
+    public String askPage() {
+        return "form/recommend";
+    }
 }

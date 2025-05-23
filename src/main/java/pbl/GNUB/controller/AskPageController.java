cbeb8f97e438eb3a9bbc51e749c43721ae52775e
hangyeol <chlgksruf3414@gmail.com>
2025-05-24 00:16:16 +0900

    askPageController

---
+++ src/main/java/pbl/GNUB/controller/AskPageController.java
@@ -0,0 +1,10 @@
+package pbl.GNUB.controller.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AskPageController {
+
+    
+}

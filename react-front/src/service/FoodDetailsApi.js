67c20c932d5b8aa0f13c1b79c77f7ef1cccbc3e3
hangyeol <chlgksruf3414@gmail.com>
2025-05-26 15:51:51 +0900

    [react] FoodDetailsApi

--- react-front/src/service/FoodDetailsApi.js
+++ react-front/src/service/FoodDetailsApi.js
@@ -5,6 +5,7 @@ const BASE = process.env.REACT_APP_API_URL; // 백엔드 API 경로
 export const fetchShopDetails = async (id) => {
   try {
     const response = await axios.get(`${BASE}/api/foodDetails/${id}`);
+    console.log("📦 API 응답:", response.data);
     return response.data;
   } catch (error) {
     console.error('❌ 음식점 상세정보 불러오기 실패:', error);
